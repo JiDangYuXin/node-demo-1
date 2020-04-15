372607df3648df929e46061ca17221f5e32af9bb
cybill <1007518429@qq.com>
2020-04-15 21:16:43 +0800

    上传github

--- server.js
+++ server.js
@@ -26,13 +26,21 @@ var server = http.createServer(function (request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈`);
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="/x">
+    </head>
+    <body>
+        <h1>我是h1标签</h1>
+    </body>
+`);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`body{color: red;}`);
-    response.end();
+    response.end(`h1{color: red;}`);
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
